19084921a07cc874d8f6016fa58dd678af8c0099
ATegtmeier <ategtmei@gmail.com>
2017-06-01 23:02:11 -0400

    Lots of code cleanup.

--- app/src/main/java/com/example/ateg/flooringmaster/AddressListActivity.java
+++ app/src/main/java/com/example/ateg/flooringmaster/AddressListActivity.java
@@ -42,117 +42,27 @@ public class AddressListActivity extends ListActivity {
     private List<Address> addresses = new ArrayList<Address>();
     private AddressDao addressDao;
 
-    //
-//    protected void setupAdapter() {
-//        //if (getActivity() == null || gridView == null) return;
-//
-//        if (addresses != null) {
-//            list.setAdapter(new GalleryItemAdapter(galleryItems));
-//            gridView.setOnScrollListener(new AbsListView.OnScrollListener() {
-//                @Override
-//                public void onScrollStateChanged(AbsListView view, int scrollState) {
-//
-//                }
-//
-//                @Override
-//                public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-//                    if (firstVisibleItem + visibleItemCount >= totalItemCount) {
-//                        loadAnotherPage();
-//                    }
-//                }
-//            });
-//        } else {
-//            gridView.setAdapter(null);
-//        }
-//    }
-//    private class AddressItemAdapter extends ArrayAdapter<Address> {
-//        public AddressItemAdapter(Context context, int resource, List<Address> objects) {
-//            super(context, resource, objects);
-//        }
-////        public AddressItemAdapter(List<Address> addressItems) {
-////            //super(this, 0, addressItems);
-////            //super();
-////        }
-//
-//        //@NonNull
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//            if (convertView == null)
-//                convertView = getLayoutInflater().inflate(R.layout.list_item_address, parent, false);
-//
-//            ListView listView = (ListView) convertView.findViewById(android.R.id.list);
-//            imageView.setImageResource(R.drawable.keep_and_moon);
-//
-//            GalleryItem galleryItem = getItem(position);
-//            thumbnailThread.queueThumbnail(imageView, galleryItem.getUrl());
-//
-//            return convertView;
-//        }
-//public AddressAdapter(List<Address> addresses) {
-//            super(getActivity(), 0, crimes);
-//
-//    @Override
-//    public void onListItemClick(ListView listView, View view, int position, long id) {
-//        Address address = ((AddressAdapter) getListAdapter()).getItem(position);
-//
-//        // Start Crime Pager Activity
-//        Intent intent = new Intent(this, CrimePagerActivity.class);
-//
-//        intent.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
-//        startActivity(intent);
-
     @Override
     public void onResume() {
         super.onResume();
         ((AddressAdapter) getListAdapter()).notifyDataSetChanged();
     }
 
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //set
-        //setHasOptionsMenu(true);
-        //getActivity().
                 setTitle(R.string.address_list);
 
         this.addressDao = new AddressDaoRemoteImpl(this, new HttpUtilities(this));
 
-        //crimes = CrimeLab.get(getActivity()).getCrimes();
-        //addresses = this.addressDao = new AddressDaoRemoteImpl(this, new HttpUtilities(this));
-
-//        CrimeAdapter adapter = new CrimeAdapter(crimes);
-//
-//        setListAdapter(adapter);
-
-
-        //setRetainInstance(true);
         new FetchAddressesTask().execute(0);
-
-//        thumbnailThread = new ThumbnailDownloader<>(new Handler());
-//        thumbnailThread.setListener(new ThumbnailDownloader.Listener<ImageView>() {
-//            @Override
-//            public void onThumbnailDownloaded(ImageView imageView, Bitmap thumbnail) {
-//                if (isVisible()){
-//                    imageView.setImageBitmap(thumbnail);
-//                }
-//            }
-//        });
-//
-//        thumbnailThread.start();
-//        thumbnailThread.getLooper();
-
-        //setActionBar(null);
 
         AddressAdapter addressAdapter = new AddressAdapter(this, 0, addresses);
         setListAdapter(addressAdapter);
 
         Log.i(TAG, "Background thread started.");
     }
+
     private class FetchAddressesTask extends AsyncTask<Integer, Void, List<Address>> {
 
         @Override
@@ -161,35 +71,18 @@ public class AddressListActivity extends ListActivity {
             List<Address> addressListResult = addressDao.list();
 
             return addressListResult;
-            //FlickrFetchr flickrFetchr = new ();
-
-//            for (Integer parameter : params) {
-//                if (parameter != null) {
-//                    flickrFetchr.setPageNumber(parameter);
-//                    break;
-//                }
-//            }
-//
-//            return flickrFetchr.fetchItems();
         }
+
         @Override
         protected void onPostExecute(List<Address> _addressItems) {
             addresses.addAll(_addressItems);
-            //setupAdapter();
         }
     }
-
-//
-
-
-//    }
-
 
     @Override
     public View onCreateView(String name, Context context, AttributeSet attrs) {
         View view = super.onCreateView(name, context, attrs);
         Log.d(TAG, name);
-       // ListView listView = (ListView) view.findViewById(android.R.id.list);
 
         return view;
     }
@@ -198,7 +91,6 @@ public class AddressListActivity extends ListActivity {
             super(context, resource, objects);
         }
 
-//        }
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             // if a view was not input inflate one.
@@ -210,227 +102,15 @@ public class AddressListActivity extends ListActivity {
             // Configure the view for the crime.
             Address address = getItem(position);
 
-            TextView titleTextView =
+            TextView nameTextView =
                     (TextView) convertView.findViewById(R.id.address_list_item_nameTextView);
-            titleTextView.setText(address.getLastName() + ", " + address.getFirstName());
-            TextView dateTextView =
+            nameTextView.setText(address.getLastName() + ", " + address.getFirstName());
+            TextView companyTextView =
                     (TextView) convertView.findViewById(R.id.address_list_item_companyTextView);
-            dateTextView.setText(address.getCompany());
+            companyTextView.setText(address.getCompany());
 
             return convertView;
         }
 
-
     }
-//
-//    @Override
-//    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View view = super.onCreateView(inflater, container, savedInstanceState);
-//
-//        ListView listView = (ListView) view.findViewById(android.R.id.list);
-//
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-//            // Use the floating context menu on froyo and gingerbread devices
-//            registerForContextMenu(listView);
-//        } else {
-//            // Use contextual action bar on honeycomb and higher
-//            listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-//            listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
-//                @Override
-//                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-//
-//                }
-//
-//                @Override
-//                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-//                    MenuInflater menuInflater = mode.getMenuInflater();
-//                    menuInflater.inflate(R.menu.crime_list_item_context, menu);
-//
-//                    return true;
-//                }
-//
-//                @Override
-//                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-//
-//                    switch (item.getItemId()) {
-//                        case R.id.menu_item_delete_crime:
-//                            CrimeAdapter crimeAdapter = (CrimeAdapter) getListAdapter();
-//                            CrimeLab crimeLab = CrimeLab.get(getActivity());
-//                            for (int i = crimeAdapter.getCount() - 1; i >= 0; i--) {
-//                                if (getListView().isItemChecked(i)) {
-//                                    crimeLab.delete(crimeAdapter.getItem(i));
-//                                }
-//                            }
-//                            mode.finish();
-//                            crimeAdapter.notifyDataSetChanged();
-//                            return true;
-//
-//                        default:
-//                            return false;
-//                    }
-//
-//                }
-//
-//                @Override
-//                public void onDestroyActionMode(ActionMode mode) {
-//
-//                }
-//            });
-//        }
-//
-//        View emptyView = (View) view.findViewById(R.id.empty_crime_list);
-//
-//        listView.setEmptyView(emptyView);
-//
-//        return view;
-//    }
-//
-//
-//
-//
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//    }
-//
-//    //onCreateV
-//    @Override
-//    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        View view = super.onCreateView(inflater, container, savedInstanceState);
-//
-//        ListView listView = (ListView) view.findViewById(android.R.id.list);
-//
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-//            // Use the floating context menu on froyo and gingerbread devices
-//            registerForContextMenu(listView);
-//        } else {
-//            // Use contextual action bar on honeycomb and higher
-//            listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-//            listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
-//                @Override
-//                public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-//
-//                }
-//
-//                @Override
-//                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-//                    MenuInflater menuInflater = mode.getMenuInflater();
-//                    menuInflater.inflate(R.menu.crime_list_item_context, menu);
-//
-//                    return true;
-//                }
-//
-//                @Override
-//                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-//                    return false;
-//                }
-//
-//                @Override
-//                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-//
-//                    switch (item.getItemId()) {
-//                        case R.id.menu_item_delete_crime:
-//                            CrimeAdapter crimeAdapter = (CrimeAdapter) getListAdapter();
-//                            CrimeLab crimeLab = CrimeLab.get(getActivity());
-//                            for (int i = crimeAdapter.getCount() - 1; i >= 0; i--) {
-//                                if (getListView().isItemChecked(i)) {
-//                                    crimeLab.delete(crimeAdapter.getItem(i));
-//                                }
-//                            }
-//                            mode.finish();
-//                            crimeAdapter.notifyDataSetChanged();
-//                            return true;
-//
-//                        default:
-//                            return false;
-//                    }
-//
-//                }
-//
-//                @Override
-//                public void onDestroyActionMode(ActionMode mode) {
-//
-//                }
-//            });
-//        }
-//
-//        View emptyView = (View) view.findViewById(R.id.empty_crime_list);
-//
-//        listView.setEmptyView(emptyView);
-//
-//        return view;
-//    }
-//
-////    }
-//
-//    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo);
-//        getActivity().getMenuInflater().inflate(R.menu.crime_list_item_context, menu);
-//    }
-//
-//    @Override
-//    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
-//        super.onCreateOptionsMenu(menu, menuInflater);
-//        menuInflater.inflate(R.menu.fragment_crime_list, menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem menuItem) {
-//        switch (menuItem.getItemId()) {
-//            case R.id.menu_item_new_crime:
-//                createCrime();
-//                return true;
-//            case R.id.menu_item_show_subtitles:
-//
-//                Activity activity = getActivity();
-//                ActionBar actionBar = activity.getActionBar();
-//
-//                if (actionBar != null) {
-//
-//                    if (actionBar.getSubtitle() == null) {
-//                        actionBar.setSubtitle(R.string.subtitle);
-//                        menuItem.setTitle(R.string.hide_subtitle);
-//                    } else {
-//                        actionBar.setSubtitle(null);
-//                        menuItem.setTitle(R.string.show_subtitle);
-//                    }
-//                }
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(menuItem);
-//        }
-//
-//    }
-//
-//    private void createCrime() {
-//        Crime crime = new Crime();
-//        CrimeLab.get(getActivity()).create(crime);
-//        Intent intent = new Intent(getActivity(), CrimePagerActivity.class);
-//        intent.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getId());
-//        startActivityForResult(intent, 0);
-//    }
-//
-//    @Override
-//    public boolean onContextItemSelected(MenuItem item) {
-//
-//        AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-//        int position = adapterContextMenuInfo.position;
-//        CrimeAdapter crimeAdapter = (CrimeAdapter) getListAdapter();
-//        Crime crime = crimeAdapter.getItem(position);
-//
-//        switch (item.getItemId()) {
-//            case R.id.menu_item_delete_crime:
-//                CrimeLab.get(getActivity()).delete(crime);
-//                crimeAdapter.notifyDataSetChanged();
-//                return true;
-//        }
-//
-//        return super.onContextItemSelected(item);
-//    }
 }
