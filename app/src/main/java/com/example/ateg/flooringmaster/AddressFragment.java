61c2218c949afc8dcc17f3b98bbb363400ac6060
ATegtmeier <ategtmei@gmail.com>
2017-07-12 19:55:44 -0400

    First tire kicking.

--- app/src/main/java/com/example/ateg/flooringmaster/AddressFragment.java
+++ app/src/main/java/com/example/ateg/flooringmaster/AddressFragment.java
@@ -30,9 +30,7 @@ public class AddressFragment extends BaseFragment<AddressPresenter> implements A
     private static final String TAG = "Address Fragment";
     private static final String ACTION_FOR_INTENT_CALLBACK = "THIS_IS_A_UNIQUE_KEY_WE_USE_TO_COMMUNICATE";
 
-    //private Address address;
     private ProgressDialog mLoadingDialog;
-    //private AddressClient addressClient;
     private AddressDao addressDao;
     private Integer id;
 
@@ -53,36 +51,16 @@ public class AddressFragment extends BaseFragment<AddressPresenter> implements A
         Uri baseUri = Uri.parse(getString(R.string.starting_root_url));
         addressDao = new AddressDaoRemoteImpl(getActivity(), new HttpUtilities(getActivity(), baseUri));
 
-        addressClient = new AddressClient(getActivity(),
-                addressDao,
-                ACTION_FOR_INTENT_CALLBACK);
-
-        Integer id = (Integer) getArguments().getSerializable(EXTRA_ADDRESS_ID);
-        //address = null;
-        getContent(id);
+        id = (Integer) getArguments().getSerializable(EXTRA_ADDRESS_ID);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        getActivity().registerReceiver(receiver, new IntentFilter(ACTION_FOR_INTENT_CALLBACK));
+    protected int layout() {
+        return R.layout.fragment_address;
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(receiver);
-    }
-
-    private void getContent(Integer id) {
-        addressClient.get(id);
-        mLoadingDialog = ProgressDialog.show(getActivity(), "Getting Data ...", "Waiting For Results...", true);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflator, ViewGroup parent, Bundle savedInstanceState) {
-        View view = inflator.inflate(R.layout.fragment_address, parent, false);
-
+    protected void setUi(View v) {
         if (NavUtils.getParentActivityName(getActivity()) != null) {
             Activity activity = getActivity();
             if (activity != null) {
@@ -91,39 +69,12 @@ public class AddressFragment extends BaseFragment<AddressPresenter> implements A
                     actionBar.setDisplayHomeAsUpEnabled(true);
             }
         }
-
-        return view;
-    }
-
-    @Override
-    protected int layout() {
-        return 0;
-    }
-
-    @Override
-    protected void setUi(View v) {
-
     }
 
     @Override
     protected void init() {
 
     }
-
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // clear the progress indicator
-            if (mLoadingDialog != null) {
-                mLoadingDialog.dismiss();
-            }
-
-            address = (Address) intent.getSerializableExtra(AddressClient.ADDRESS_SERIALIZABLE_EXTRA);
-
-            //updateAddress();
-            Toast.makeText(context, "Address: " + address.getFullName(), Toast.LENGTH_SHORT).show();
-        }
-    };
 
     @Override
     protected void populate() {
@@ -142,20 +93,21 @@ public class AddressFragment extends BaseFragment<AddressPresenter> implements A
 
     @Override
     public void showLoading() {
-        mLoadingDialog.show();
         mLoadingDialog = ProgressDialog.show(getActivity(), "Getting Data ...", "Waiting For Results...", true);
     }
 
     @Override
     public void showError(Throwable e) {
-        mLoadingDialog.dismiss();
-        //showToast(e.getMessage());
+        if (mLoadingDialog != null)
+            mLoadingDialog.dismiss();
+
         Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void setAddress(Address address) {
-        mLoadingDialog.dismiss();
+        if (mLoadingDialog != null)
+            mLoadingDialog.dismiss();
         // Populate view with user data
     }
 }
