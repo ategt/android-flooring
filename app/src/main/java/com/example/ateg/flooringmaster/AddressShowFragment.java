b0540c5501974a16aa622dbb2b5dc0723e3c1426
ATegtmeier <ategtmei@gmail.com>
2017-07-19 17:08:01 -0400

    Address Show MVP needs to be fully implemented and show and create MVPs need tested.

--- app/src/main/java/com/example/ateg/flooringmaster/AddressShowFragment.java
+++ app/src/main/java/com/example/ateg/flooringmaster/AddressShowFragment.java
@@ -1,13 +1,19 @@
 package com.example.ateg.flooringmaster;
 
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by ATeg on 7/19/2017.
  */
 
-public class AddressShowFragment extends BaseFragment<AddressShowPresenter> {
+public class AddressShowFragment extends BaseFragment<AddressShowPresenter> implements AddressShowView {
     public static final String ADDRESS_ID_TO_SHOW = "com.example.ateg.flooringmaster.AddressShowFragment.ADDRESS_ID_TO_SHOW";
+    public static final String TAG = "Address Show Fragment";
+
+    private ProgressDialog mLoadingDialog;
 
     @Override
     protected int layout() {
@@ -36,6 +42,25 @@ public class AddressShowFragment extends BaseFragment<AddressShowPresenter> {
 
     @Override
     protected AddressShowPresenter createPresenter() {
-        return null;
+        return new AddressShowPresenter(this);
+    }
+
+    @Override
+    public void showError(Throwable ex) {
+        if (mLoadingDialog != null)
+            mLoadingDialog.dismiss();
+        Log.e(TAG, "Error occured.", ex);
+        Toast.makeText(getActivity(), "Error Ocurred!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void showLoading(Integer id) {
+
+    }
+
+    @Override
+    public void displayAddress(Address address) {
+
     }
 }
