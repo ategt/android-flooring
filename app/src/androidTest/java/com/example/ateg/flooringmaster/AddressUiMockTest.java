71cd67e6bdc088e786dd400b59d09a828cf78180
ATegtmeier <ategtmei@gmail.com>
2017-07-24 19:54:28 -0400

    Used record espresso test option in ide.

--- app/src/androidTest/java/com/example/ateg/flooringmaster/AddressUiMockTest.java
+++ app/src/androidTest/java/com/example/ateg/flooringmaster/AddressUiMockTest.java
@@ -6,16 +6,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.espresso.matcher.CursorMatchers;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
+
+import junit.framework.Assert;
 
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
@@ -65,22 +71,15 @@ public class AddressUiMockTest {
             AddressDao addressDao = mock(AddressDao.class);
 
             ResultProperties resultProperties = new ResultProperties(AddressSortByEnum.SORT_BY_COMPANY, 0, 25);
+            ResultProperties resultProperties1 = new ResultProperties(AddressSortByEnum.SORT_BY_COMPANY, 1, 25);
+            ResultProperties resultProperties2 = new ResultProperties(AddressSortByEnum.SORT_BY_COMPANY, 2, 25);
 
-            when(addressDao.list(resultProperties)).then(new Answer<List<Address>>() {
-                @Override
-                public List<Address> answer(InvocationOnMock invocation) throws Throwable {
-                    Thread.sleep(500);
-                    return resultsFromList;
-                }
-            });
+            when(addressDao.list(resultProperties)).thenReturn(resultsFromList);
+            when(addressDao.list(any(ResultProperties.class))).thenReturn(resultsFromList);
 
-            when(addressDao.list()).then(new Answer<List<Address>>() {
-                @Override
-                public List<Address> answer(InvocationOnMock invocation) throws Throwable {
-                    Thread.sleep(500);
-                    return resultsFromList;
-                }
-            });
+            when(addressDao.list(resultProperties1)).thenReturn(resultsFromList);
+            when(addressDao.list(resultProperties2)).thenReturn(resultsFromList);
+            when(addressDao.list()).thenReturn(resultsFromList);
 
             AddressDaoSingleton.setAddressDao(addressDao);
         }
@@ -188,29 +187,29 @@ public class AddressUiMockTest {
 
     @Test
     public void simpleIndexWithMany() throws IOException {
-        Address address = new Address();
+        final Address firstAddress = new Address();
 
-        address.setCompany("Company A");
-        address.setFirstName("Bill");
-        address.setLastName("Billerston");
+        firstAddress.setCompany("Company A");
+        firstAddress.setFirstName("Bill");
+        firstAddress.setLastName("Billerston");
 
-        resultsFromList.add(address);
+        resultsFromList.add(firstAddress);
 
-        address = new Address();
+        final Address secondAddress = new Address();
 
-        address.setCompany("Levis");
-        address.setFirstName("Levi");
-        address.setLastName("Pants");
+        secondAddress.setCompany("Levis");
+        secondAddress.setFirstName("Levi");
+        secondAddress.setLastName("Pants");
 
-        resultsFromList.add(address);
+        resultsFromList.add(secondAddress);
 
-        address = new Address();
+        final Address thirdAddress = new Address();
 
-        address.setCompany("Company B");
-        address.setFirstName("Bill");
-        address.setLastName("Billerston");
+        thirdAddress.setCompany("Company B");
+        thirdAddress.setFirstName("Bill");
+        thirdAddress.setLastName("Billerston");
 
-        resultsFromList.add(address);
+        resultsFromList.add(thirdAddress);
 
         Random random = new Random();
 
@@ -234,6 +233,12 @@ public class AddressUiMockTest {
 
         mainActivityActivityTestRule.launchActivity(new Intent());
 
+        AddressDao addressDao = AddressDaoSingleton.getAddressDao(null);
+
+        List<Address> addresses = addressDao.list(new ResultProperties(AddressSortByEnum.SORT_BY_COMPANY, 0, 25));
+
+        Assert.assertEquals(addresses.size(), 3004);
+
         Espresso.onView(ViewMatchers.withId(R.id.address_index_listView))
                 .perform(ViewActions.swipeUp())
                 .perform(ViewActions.swipeUp())
@@ -249,6 +254,84 @@ public class AddressUiMockTest {
 //        Espresso.onView(ViewMatchers.withId(R.id.address_index_listView))
 //                .check()
 
+        //Espresso.onData(ArgumentMatchers.anyList())
+                //.
+
+//        Espresso.onView(ViewMatchers.withId(R.id.address_index_listView))
+//                .
+
+        DataInteraction firstRowInteraction = Espresso.onData(CursorMatchers.withRowString(new org.hamcrest.Matcher() {
+            @Override
+            public void describeTo(Description description) {
+
+            }
+
+            @Override
+            public boolean matches(Object item) {
+                return true;
+            }
+
+            @Override
+            public void describeMismatch(Object item, Description mismatchDescription) {
+
+            }
+
+            @Override
+            public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+
+            }
+        }, new org.hamcrest.Matcher<String>() {
+            @Override
+            public boolean matches(Object item) {
+                return firstAddress.equals(item);
+            }
+
+            @Override
+            public void describeMismatch(Object item, Description mismatchDescription) {
+
+            }
+
+            @Override
+            public void _dont_implement_Matcher___instead_extend_BaseMatcher_() {
+
+            }
+
+            @Override
+            public void describeTo(Description description) {
+
+            }
+        }));
+
+        ViewInteraction firstRowInteractionb = firstRowInteraction.perform(ViewActions.scrollTo());
+        firstRowInteractionb.check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        DataInteraction thing = Espresso.onData(getDataMatcher(secondAddress));
+                thing.perform(ViewActions.scrollTo());
+                thing.check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        Espresso.onData(getDataMatcher(firstAddress))
+                .atPosition(1);
+
+        Espresso.onData(getDataMatcher(secondAddress))
+                .atPosition(2);
+
+        Espresso.onData(getDataMatcher(secondAddress))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+        Espresso.onData(getDataMatcher(secondAddress))
+                .perform(ViewActions.scrollTo())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+            //.check(ViewAssertions.matches(ViewMatchers.withText(org.hamcrest.Matcher<String>.)));
+
+        Espresso.onData(getDataMatcher(thirdAddress))
+                .atPosition(3004);
+
+        Espresso.onData(getDataMatcher(lastAddress))
+                .atPosition(3004);
+        Espresso.onData(getDataMatcher(lastAddress))
+                .check(ViewAssertions.matches(ViewMatchers.hasSibling(ViewMatchers.withId(R.id.address_list_item_nameTextView))));
+        //.check(ViewAssertions.matches(ViewMatchers.));
+        //.check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         Espresso.onData(getDataMatcher(lastAddress)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
         Espresso.onData(getDataMatcher(lastAddress)).check(ViewAssertions.matches(ViewMatchers.isClickable()));
 
