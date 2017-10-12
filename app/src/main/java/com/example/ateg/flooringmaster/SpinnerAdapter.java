b92098085c121642a416d57840bd94c4ad584bfd
Adam Tegtmeier <ategtmei@gmail.com>
2017-10-12 19:32:50 -0400

    This is different. Still do not like it, but it has color now.

--- app/src/main/java/com/example/ateg/flooringmaster/SpinnerAdapter.java
+++ app/src/main/java/com/example/ateg/flooringmaster/SpinnerAdapter.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 /**
  * Created by ATeg on 10/12/2017.
@@ -21,6 +22,14 @@ public class SpinnerAdapter extends ArrayAdapter<AddressSearchByOptionEnum> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        TextView textView = (TextView) convertView;
+
+        AddressSearchByOptionEnum addressSearchByOptionEnum
+                = AddressSearchByOptionEnum.parse(String.valueOf(textView.getText()));
+
+        String content = addressSearchByOptionEnum.ordinal() + " - " + addressSearchByOptionEnum.toString();
+        textView.setText(content);
+
+        return textView;
     }
 }
